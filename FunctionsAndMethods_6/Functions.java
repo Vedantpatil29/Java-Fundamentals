cc73270d2f6ec120aed89a8458307fcb7e772d7b
Vedantpatil29 <vedantpatil2918@gmail.com>
2026-01-26 17:55:10 +0530

    added new file

---
+++ FunctionsAndMethods_6/Functions.java
@@ -0,0 +1,12 @@
+public class Functions{
+    public static void PrintHelloWorld(){
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        return; //void give 0 return therefore return;
+    }
+    public static void main(String args[]){
+        PrintHelloWorld();
+    }
+}
