5e7be0e94665ee664527301859b540cad86cf65e
Vedantpatil29 <vedantpatil2918@gmail.com>
2026-01-29 23:43:17 +0530

    added new file

---
+++ Arrays(Part1)_7/Pair_array.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Pair_array{
+    public static void PairArr(int numbers[]){
+        int tp = 0;
+        for(int i = 0 ; i<numbers.length ; i++){
+            int curr = numbers[i];
+            for(int j = i+1 ; j<numbers.length ; j++){
+                System.out.print("(" + curr + " , " + numbers[j] + ")  ");
+                tp++;
+            }
+            System.out.println("");
+            
+        }
+        System.out.println("total pairs in array is = " + tp);
+    }
+
+    public static void main(String args[]){
+        int numbers[] = {2,4,6,8,10,12};
+        PairArr(numbers);
+    }
+}
