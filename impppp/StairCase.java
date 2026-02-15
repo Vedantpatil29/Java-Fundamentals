3db1fa14dcc89a763af2f30fe8bc14a0f8fb31ac
Vedantpatil29 <vedantpatil2918@gmail.com>
2026-02-15 20:53:00 +0530

    added new file

---
+++ impppp/StairCase.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class StairCaseSearch{
+
+    public static boolean search(int matrix[][] , int key){  //using (0,3) position
+        int row = 0; 
+        int col = matrix[0].length-1;
+
+        while(row<matrix.length && col>=0){
+            if(key == matrix[row][col]){
+                System.out.println("key is found in (" + row + "," + col + ")");
+                return true;
+            }
+
+            else if(key < matrix[row][col]){
+                col--;    
+            }
+
+            else {
+                row++;
+            }
+           
+        }
+         System.out.println("key not found!");
+
+            return false;
+
+    }
+
+        public static void main(String args[]){
+            int matrix[][] = {{10,20,30,40},
+                              {15,25,35,45} ,
+                              {27,29,37,48},
+                              {32,33,39,50}};
+
+           int key = 99;
+
+           search(matrix,key);
+        }
+}
